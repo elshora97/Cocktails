dbf90d71ae4bf29c94206c1d914dada2e1f71179
elshora97 <ahmedashrafelshora@gmail.com>
2023-03-16 17:03:47 +0200

    Creating NavBar Comp. & About us and error pages4

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        distinctio doloribus at perferendis repellat cum, suscipit nulla harum
+        quos, quaerat ex nostrum eligendi doloremque animi, amet illum porro.
+        Mollitia fugit vel obcaecati hic! Recusandae iste molestias tenetur
+        praesentium et aliquid!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
