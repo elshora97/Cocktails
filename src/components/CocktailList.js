289b2e78f114b42e453ea2229a21b493886778af
elshora97 <ahmedashrafelshora@gmail.com>
2023-03-16 20:22:38 +0200

    Displat Cocktails

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No Result Matches your search</h2>;
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default CocktailList
+export default CocktailList;
